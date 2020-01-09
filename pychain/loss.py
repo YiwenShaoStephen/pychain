fix the final-probs issue for numerator computation

pychain/loss.py
@@ -1,4 +1,5 @@
 # Copyright       2019 Yiwen Shao
+#                 2020 Yiming Wang
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -35,6 +36,8 @@ class ChainFunction(torch.autograd.Function):
         backward_transition_probs = graphs.backward_transition_probs
         initial_probs = graphs.initial_probs
         num_states = graphs.num_states
+        final_probs = graphs.final_probs
+        leaky_hmm_coefficient = graphs.leaky_hmm_coefficient
         objf, input_grad, _ = pychain_C.forward_backward(
             forward_transitions,
             forward_transition_indices,
@@ -42,7 +45,8 @@ class ChainFunction(torch.autograd.Function):
             backward_transitions,
             backward_transition_indices,
             backward_transition_probs,
-            initial_probs, exp_input, num_states, graphs.leaky_hmm_coefficient)
+            initial_probs, final_probs,
+            exp_input, num_states, leaky_hmm_coefficient)
         ctx.save_for_backward(input_grad)
         return objf
 